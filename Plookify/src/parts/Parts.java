f2837d916fcd9892c1cd4af91cfd3f6fefbfb83b
ec15014 <e.o.mensah@se15.qmul.ac.uk>
2017-02-21 10:41:15 +0000

    Parts class updated

--- Plookify/src/parts/Parts.java
+++ Plookify/src/parts/Parts.java
@@ -5,6 +5,8 @@
  */
 package parts;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
@@ -13,19 +15,60 @@ import java.util.*;
  */
 public class Parts {
    private String name, description;
-   private int ID, stockLevel, cost;
-   private Date installationDate, warrantyExpiry;
+   private int ID, cost;
+   private int stockLevel = 10;
+   private Calendar installationDate, warrantyExpiry;
+   private final DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+   public Parts(String name, String description, int cost){
+       this.name = name;
+       this.description = description;
+       this.cost = cost;
+       
+   }
+   public int getID(){
+       return ID;
+   }
    
-   public Parts(){
+   public Calendar getInstDate(){
+       return installationDate;
+   }
+   
+   public Calendar getWarrExpDate(){
+       return warrantyExpiry;
+   }
+   
+   public void setInstDate(Calendar date){      
+       date = Calendar.getInstance();     
+       installationDate = date;    
+   }
+   
+   public void setWarrExpDate(){
        
        
+   }
+   
+   public String getName(){
+       return name;
+   }
+   
+   public String getDesc(){
+       return description;
+   }
+   
+   public int getCost(){
+       return cost;
+   }
+   
+   public int getStockLvl(){
+       return stockLevel;
    }
    public void withdrawPart(){
        
    }
    
-   public void addPart(){
-       
+   public void addPart(String name, String desc, int cost){
+        Parts newPart = new Parts(name,desc,cost);
+        
    }
    
    public List<Parts> getPartsUsed(){
@@ -46,6 +89,14 @@ public class Parts {
    
    public void searchByRegNum(String regNumber){
        
+   }
+   
+   public void incStockLevel(){
+       stockLevel++;
+   }
+   
+   public void decStockLevel(){
+       stockLevel--;
    }
 }
 
