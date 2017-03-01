5bb02b9d8043d85ef991afedb199476235185455
pro <pro@100.66.52.123>
2017-03-01 00:07:49 -0800

    Merge origin/master into master
    Conflicts:
    	.gitignore

--- NetBeansProjects/SE16/Plookify/src/specRep/gui/specialistRepairGUI.java
+++ NetBeansProjects/SE16/Plookify/src/specRep/gui/specialistRepairGUI.java
@@ -69,6 +69,11 @@ public class specialistRepairGUI extends javax.swing.JFrame {
         jPanel1.add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 130, -1, -1));
 
         jButton4.setText("exit");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -120,6 +125,10 @@ public class specialistRepairGUI extends javax.swing.JFrame {
     private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton1MouseClicked
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        System
+    }//GEN-LAST:event_jButton4ActionPerformed
 
     /**
      * @param args the command line arguments
