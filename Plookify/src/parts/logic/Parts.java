3036255d64252acf20a98f2b6beb4d56d745825c
ec15014 <e.o.mensah@se15.qmul.ac.uk>
2017-03-14 01:03:28 +0000

    updated parts class

--- Plookify/src/parts/logic/Parts.java
+++ Plookify/src/parts/logic/Parts.java
@@ -1,10 +1,14 @@
-/*
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package parts.logic;
 
+import common.Database;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -16,15 +20,14 @@ import java.util.*;
 public class Parts {
    private String name, description;
    private int ID, cost;
-   private int stockLevel = 10;
+   private int stockLevel = 0;
    private Calendar installationDate, warrantyExpiry;
    private final DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-   public Parts(String name, String description, int cost){
-       this.name = name;
-       this.description = description;
-       this.cost = cost;
+   
+   public Parts(){
        
    }
+   
    public int getID(){
        return ID;
    }
@@ -67,9 +70,40 @@ public class Parts {
    }
    
    public void addPart(String name, String desc, int cost){
-        Parts newPart = new Parts(name,desc,cost);
         
-   }
+        Database db = new Database();
+        db.connect();
+
+        try {                
+                Statement statement;
+                incStockLevel();
+                String query = "INSERT INTO PartsRecord VALUES ('"+name+"', '"+desc+"','"+stockLevel+"','"+cost+"');";
+                //System.out.println("Query :" + query);
+                statement = db.getConnection().createStatement();
+                statement.setQueryTimeout(10);
+                
+                //statement.executeUpdate("drop table if exists 'VehicleRecords'");
+                		
+
+                statement.executeUpdate(query);
+
+                ResultSet vr = statement.executeQuery("select * from 'PartsRecord'");
+
+		System.out.println("PartID PartName PartDesc");
+		while(vr.next()){
+			System.out.println(vr.getInt("ID")+"	   "+vr.getString("Name")+"	        "+vr.getString("Description"));
+		}
+                System.out.println("Successfully inserted Part");
+
+                vr.close();
+        }
+	catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+	}
+        db.closeConn();
+    }
+        
+   
    
    public List<Parts> getPartsUsed(){
        return null;       
@@ -99,4 +133,5 @@ public class Parts {
        stockLevel--;
    }
 }
+
 
